8950f4b4fd5420d4de22ee22bf58742d760fb823
SyahriRhamadhan <2001020038@student.umrah.ac.id>
2025-04-09 11:42:27 +0700

    user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            [
+                'name' => 'Admin Demo',
+                'email' => 'admin@example.com',
+                'password' => Hash::make('123'),
+                'email_verified_at' => now(),
+                'remember_token' => Str::random(10),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'User Demo',
+                'email' => 'user@example.com',
+                'password' => Hash::make('123'),
+                'email_verified_at' => now(),
+                'remember_token' => Str::random(10),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
+    }
+}
